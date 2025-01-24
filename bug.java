46461e74cb14101034c9e2c9558af5be12e35bbf
BugHunter-X <askzenai@gmail.com>
2025-01-24 02:22:05 +0100

    Add bug.java file

---
+++ bug.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int index = 10;
+
+        try {
+            System.out.println(arr[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException caught.");
+        } finally {
+            System.out.println("This will always execute.");
+        }
+    }
+}
